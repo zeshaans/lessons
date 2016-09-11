89d04238d7520b79982b93182c52d2a133e38842
zeshaans <zeshaan.shamsi@gmail.com>
2016-09-11 18:00:32 +0100

    Add files via upload

---
+++ new_ruby_file.rb
@@ -0,0 +1,7 @@
+#this is my first ruby output
+puts "Hello, world!"
+puts "another new line put here by puts"
+puts "and more!"
+
+print "My name is ZeShaan/n"
+print "I'm enjoying this first lesson, it's very informative "
